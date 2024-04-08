8adaa61d05d94d470c0fc88b229e01d04b6ab12a
Nahuel Luca <nahuelluca20@gmail.com>
2024-04-07 21:29:01 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,5 +6,18 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [remixCloudflareDevProxy(), remix(), tsconfigPaths()],
+  plugins: [
+    {
+      name: "import-meta-env-ssr",
+      enforce: "pre",
+      transform(code, id, options) {
+        if (!options?.ssr && code.includes("import.meta.env.SSR")) {
+          return code.replace(/import.meta.env.SSR/g, "false");
+        }
+      },
+    },
+    remixCloudflareDevProxy(),
+    remix(),
+    tsconfigPaths(),
+  ],
 });
